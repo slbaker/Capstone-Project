a6289ee0e614f46d09201181829cfbd8ffcbaaa7
ShannonB <bakerandco@gmail.com>
2015-03-01 19:08:29 -0800

    V1 for Capstone

---
+++ Capstone V1/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.averageLabel = new System.Windows.Forms.Label();
+            this.minLabel = new System.Windows.Forms.Label();
+            this.maxLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 134);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TotalLabel1";
+            // 
+            // averageLabel
+            // 
+            this.averageLabel.AutoSize = true;
+            this.averageLabel.Location = new System.Drawing.Point(12, 206);
+            this.averageLabel.Name = "averageLabel";
+            this.averageLabel.Size = new System.Drawing.Size(35, 13);
+            this.averageLabel.TabIndex = 2;
+            this.averageLabel.Text = "label2";
+            // 
+            // minLabel
+            // 
+            this.minLabel.AutoSize = true;
+            this.minLabel.Location = new System.Drawing.Point(169, 180);
+            this.minLabel.Name = "minLabel";
+            this.minLabel.Size = new System.Drawing.Size(35, 13);
+            this.minLabel.TabIndex = 3;
+            this.minLabel.Text = "label2";
+            // 
+            // maxLabel
+            // 
+            this.maxLabel.AutoSize = true;
+            this.maxLabel.Location = new System.Drawing.Point(169, 206);
+            this.maxLabel.Name = "maxLabel";
+            this.maxLabel.Size = new System.Drawing.Size(35, 13);
+            this.maxLabel.TabIndex = 4;
+            this.maxLabel.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.maxLabel);
+            this.Controls.Add(this.minLabel);
+            this.Controls.Add(this.averageLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label averageLabel;
+        private System.Windows.Forms.Label minLabel;
+        private System.Windows.Forms.Label maxLabel;
+    }
+}
+
---
+++ Capstone V1/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+        { // C:\Users\shannon\Programming\Solutions to Programming Problems\chapter07\Program7_1\Program7_1\Sales.txt
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string input = null;
+            double total = 0; 
+            using (StreamReader Sales = File.OpenText(@"C:\\users\shannon\Programming\Solutions to Programming Problems\chapter07\Program7_1\Program7_1\Sales.txt"))
+            {
+                while ((input = Sales.ReadLine()) != null)
+                {
+                    listBox1.Items.Add(input);
+                    total += Convert.ToDouble(input);
+                }
+                Sales.Close();
+            }
+            label1.Text = total.ToString();
+            string[] salesArray = new string[7];
+            listBox1.Items.CopyTo(salesArray,0);
+            averageLabel.Text = Average(salesArray).ToString("C");
+
+            minLabel.Text = salesArray.Min();
+            maxLabel.Text = salesArray.Max();
+        }
+
+        public double Average(string[] array)
+        {
+            double total = 0;
+            foreach (string s in array)
+            {
+                total += Convert.ToDouble(s);              
+            }
+
+            double average = total/array.Length;
+            return average;
+        }
+
+    }
+}
