63b2b6242cd3c94718a19d30ac4e5d7c3ffec022
ShannonB <bakerandco@gmail.com>
2015-03-01 20:25:53 -0800

    First successful unit test yay

--- Capstone V3/CapstoneSolution/BookDomainTest/BookTests.cs
+++ Capstone V3/CapstoneSolution/BookDomainTest/BookTests.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using BookDomain;
+
 namespace BookDomainTest
 {
     [TestClass]
     public class BookTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SimpleBookTest()
         {
+            Book b = new Book();
+            b.ID = 1234567890;
+            b.Title = "Fred Flintstone";
+            b.Subject = "Bedrock in photos";
+            b.AuthorID = 1;
+            b.YearPublished = 1;
+
+            Assert.IsNotNull(b.Title);
+            Assert.AreEqual("Fred Flintstone", b.Title);
         }
     }
 }
