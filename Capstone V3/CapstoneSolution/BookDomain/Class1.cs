f68dbf827f91c4c3cea3b131591b6caacaa4a196
ShannonB <bakerandco@gmail.com>
2015-03-01 20:17:13 -0800

    Added first V3 project, domain library and skeleton tests

---
+++ Capstone V3/CapstoneSolution/BookDomain/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookDomain
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public int AuthorID { get; set; }
+        public string Subject { get; set; }
+        public int YearPublished { get; set; }
+    }
+}
---
+++ Capstone V3/CapstoneSolution/BookDomainTest/BookTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookDomainTest
+{
+    [TestClass]
+    public class BookTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
